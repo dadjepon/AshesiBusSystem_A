4d0449b3546f30f2e8c5703191162d4b5b857a66
Kwakzz <46132576+Kwakzz@users.noreply.github.com>
2023-12-12 22:18:13 +0000

    Update driver.dart

--- ashesi_bus/lib/requests/driver.dart
+++ ashesi_bus/lib/requests/driver.dart
@@ -3,6 +3,8 @@ import 'get.dart';
 
 String _path = '/driver/';
 
+
+/// This function send a POST request for driver login. The driver logs in with their driver ID.
 Future<Map<String, dynamic>> loginDriver (
 
   String driverAshesiId,
@@ -16,6 +18,7 @@ Future<Map<String, dynamic>> loginDriver (
 }
 
 
+/// This function sends a GET request to obtain all drivers.
 Future<Map<String,dynamic>> getDrivers () async {
   
     return fetchMap(
@@ -26,6 +29,7 @@ Future<Map<String,dynamic>> getDrivers () async {
 }
 
 
+/// This function sends a GET request to obtain a driver by his/her ID.
 Future<Map<String,dynamic>> getDriverById (int driverId) async {
   
     return fetchMap(
