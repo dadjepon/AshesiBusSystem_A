45999b7448507eacbe032d623e5a44b596bc48c0
Kwakzz <46132576+Kwakzz@users.noreply.github.com>
2023-12-12 22:35:21 +0000

    Update vehicle.dart

--- ashesi_bus/lib/requests/vehicle.dart
+++ ashesi_bus/lib/requests/vehicle.dart
@@ -2,6 +2,10 @@ import 'get.dart';
 
 String _path = '/vehicle/';
 
+
+/// Retrieves a list of all vehicles
+/// Returns a Future that resolves to a List of Maps, where each Map represents a vehicle and contains
+/// vehicle information in key-value pairs
 Future<List<Map<String,dynamic>>> getAllVehicles () async {
   
     return fetchListWithQueryParams(
