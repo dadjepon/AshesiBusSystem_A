02a230bf99dbe6dadad57517511ed42ee26d2c58
Kwakzz <46132576+Kwakzz@users.noreply.github.com>
2023-12-12 22:37:49 +0000

    Update string.dart

--- ashesi_bus/lib/helper/functions/string.dart
+++ ashesi_bus/lib/helper/functions/string.dart
@@ -1,3 +1,5 @@
+/// Get last word from string. Used to get the final destination from a trip's name.
+/// For example, if the trip name is "Kumasi - Accra", this function will return "Accra"
 String getLastWord (String string) {
   List<String> words = string.split('-');
 
