4ca96cadb19f376969c3ef5c8a92e6c2e9e05b00
Kwakzz <46132576+Kwakzz@users.noreply.github.com>
2023-12-12 21:35:37 +0000

    Delete abs_app directory

--- abs_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AbsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abs_app'
--- abs_app/urls/bus_user_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from abs_app.views.bus_user_view import register_and_verify, activate, login
-
-
-urlpatterns = [
-    path('bus_user/register/', register_and_verify, name='register_and_verify'),
-    path('bus_user/activate/', activate, name='activate'),
-    path('bus_user/login/', login, name='login'),
-]
